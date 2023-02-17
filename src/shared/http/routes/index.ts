bad73ef9f01100df2607a4af2d496c8f3090759d
Bruno Pereira <brunosanctus@outlook.com>
2023-02-17 15:25:40 -0300

    created services, entities and repositories referer forgot token

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import userRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
+import passwordRoutes from '@modules/users/routes/password.routes';
 
 const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRoutes);
 
 export default routes;
