e2db1b5df473995bb956ad45568992434df331d9
Bruno Santos <brunosanctus@outlook.com>
2023-02-21 19:02:48 -0300

    feat: implemented and tested feature of send email fake with view using template handlebars

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
 
 interface IRequest {
   email: string;
@@ -20,6 +21,12 @@ class SendForgotPasswordEmailService {
     }
 
     const { token } = await userTokenRepository.generate(user.id);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await EtherealMail.sendMail({
       to: {
@@ -28,10 +35,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[SALES API] Recuperação de Senha',
       templateData: {
-        template: `Olá, {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
