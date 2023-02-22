ef9fafefa50c84da0b88c946007344395d850f2a
Bruno Pereira <brunosanctus@outlook.com>
2023-02-22 18:01:27 -0300

    feat: implemented methods of create, update, show, list and delete customers

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import userRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRoutes from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
+import customerRouter from '@modules/customers/routes/cutomer.routes';
 
 const routes = Router();
 routes.use('/products', productsRouter);
@@ -11,5 +12,6 @@ routes.use('/users', userRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRoutes);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customerRouter);
 
 export default routes;
