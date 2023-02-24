8ba7ebb2862da4b690b6af369d574578d7d175a9
Bruno Pereira <brunosanctus@outlook.com>
2023-02-24 09:21:10 -0300

    feat: implemented buying process

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -5,6 +5,7 @@ import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRoutes from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
 import customerRouter from '@modules/customers/routes/cutomer.routes';
+import ordersRouter from '@modules/orders/routes/order.routes';
 
 const routes = Router();
 routes.use('/products', productsRouter);
@@ -13,5 +14,6 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRoutes);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customerRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
